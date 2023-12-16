829dbdd8504923280ddc69785fa8db160c8a7181
Casey Baggz <caseybaggz@gmail.com>
2023-12-16 16:01:34 -0700

    Add @astrojs/markdoc package and integration Update Astro config to include markdoc integration Update VSCode settings for Astro files Update signup form HTML Update EarlyAccessForm component styles Add @astrojs/markdoc to pnpm dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [markdoc()]
 });
