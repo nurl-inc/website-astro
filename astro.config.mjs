113f9af8462c28fc0a36b14396c373ee9a020107
Casey Baggz <caseybaggz@gmail.com>
2023-12-27 10:48:44 -0600

    Add gtag.js reference and dataLayer interface Update VSCode settings and add cSpell words Update tsconfig.json with jsx and jsxImportSource options Update package.json with new dependencies Add partytown and solid-js integrations to astro.config.mjs Update index.astro with new imports and descriptions Update Layout.astro with analytics script and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 import markdoc from "@astrojs/markdoc";
+import partytown from "@astrojs/partytown";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [markdoc()],
+  integrations: [markdoc(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), solidJs()]
 });
---
+++ src/components/PlayerForm.jsx
@@ -0,0 +1,59 @@
+import { Show, createSignal } from "solid-js";
+
+export function PlayerForm() {
+  const [playerType, setPlayerType] = createSignal("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setPlayerType(event.currentTarget.value);
+  }
+
+  return (
+    <div class="center">
+      <h4 class="center">What type of player are you?</h4>
+      <Show
+        when={playerType() === ""}
+        fallback={
+          <p class="questionnaire-result">
+            <Show
+              when={playerType() === "player"}
+              fallback={
+                <>
+                  You have enough to worry about, let Nurl help with the rest.
+                  <br />
+                  <br />
+                  We give you the tools to create and manage your groups, chat,
+                  take private and public notes, and use tools from the
+                  extension marketplace to enhance your game experience.
+                  <br />
+                  <br />
+                  We even use AI to enhance those tools and make them more
+                  useful.
+                </>
+              }
+            >
+              Now you can focus on your character and telling their story.
+              <br />
+              <br />
+              We give you (and your group) a place to chat, take public notes,
+              and use tools from the extension marketplace to enhance your game
+              experience.
+              <br />
+              <br />
+              We even use AI to enhance those tools and make them more useful.
+            </Show>
+          </p>
+        }
+      >
+        <div class="questionnaire">
+          <button class="btn" value="player" onClick={handleSubmit}>
+            Player
+          </button>
+          <button class="btn" value="gm" onClick={handleSubmit}>
+            GM
+          </button>
+        </div>
+      </Show>
+    </div>
+  );
+}
