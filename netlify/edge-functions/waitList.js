91960d185038c4519c2daeb8d870d5dd2ae72ee3
Casey Baggz <caseybaggz@gmail.com>
2024-02-24 10:11:59 -0600

    chore: log contact data

--- netlify/edge-functions/waitList.js
+++ netlify/edge-functions/waitList.js
@@ -14,10 +14,12 @@ export default async function joinWaitList(req) {
       subject: "You're on the list! 🚀",
       html: "<h1>Thanks for joining the waiting list!</h1><p>We'll let you know when we're ready to launch.</p><p>In the meantime, follow us on <a href='https://twitter.com/nurlapp'>Twitter</a> for updates (or any other social platform - choose your destiny).</p><p>-- <br> Nurl Team</p>",
     });
-    const { error: audienceError } = await resend.contacts.create({
+    const { data, error: audienceError } = await resend.contacts.create({
       email,
       audienceId: "be0906d7-afca-4a26-a631-98b970ff8388",
     });
+
+    console.log("new contact", email, data);
 
     if (error) {
       throw new Error(error);
