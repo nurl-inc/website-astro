bfb608fa555e30f7a49a06a8974e6fbb26f1ff62
Casey Baggz <caseybaggz@gmail.com>
2024-02-23 18:26:06 -0600

    fix: add email to waitList.js

--- netlify/edge-functions/waitList.js
+++ netlify/edge-functions/waitList.js
@@ -2,14 +2,16 @@ import { Resend } from "resend";
 
 export const config = { path: "/success" };
 
-export default async function joinWaitList() {
+export default async function joinWaitList(req) {
+  const formData = req.formData();
+  const email = formData.get("email");
+  console.log(`Email: ${email}`);
   const resendKey = Netlify.env.get("RESEND_KEY");
-  console.log("Resend Key: ", resendKey);
   const resend = new Resend(resendKey);
   try {
     const { error } = await resend.emails.send({
       from: "Nurl <noreply@nurl.app>",
-      to: ["caseybaggz@gmail.com", "admin@nurl.app"],
+      to: [email, "admin@nurl.app"],
       subject: "You're on the list! 🚀",
       html: "<h1>Thanks for joining the waiting list!</h1><p>We'll let you know when we're ready to launch.</p><p>In the meantime, follow us on <a href='https://twitter.com/nurlapp'>Twitter</a> for updates (or any other social platform - choose your destiny).</p><p>-- <br> Nurl Team</p>",
     });
