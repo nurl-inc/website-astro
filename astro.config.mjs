166625bd85993b1c7535645e62414596a3085432
caseybaggz <caseybaggz@gmail.com>
2024-08-24 11:48:09 -0500

    feat: add custom pages to sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,31 @@ export default defineConfig({
     }),
     solidJs(),
     sitemap({
+      customPages: [
+        "https://nurl.website/blog/nurl-now-on-twitch/",
+        "https://nurl.website/blog/january-bugs-rehomed/",
+        "https://nurl.website/blog/january-features/",
+        "https://nurl.website/blog/feature-focus-nuro/",
+        "https://nurl.website/blog/og-ttrpg-turns-50/",
+        "https://nurl.website/blog/netty-awards-2024-winner/",
+        "https://nurl.website/blog/multi-group-campaigns/",
+        "https://nurl.website/blog/glorified-notes-app/",
+        "https://nurl.website/blog/supercharged-group-chat/",
+        "https://nurl.website/blog/nurl-app-not-just-for-dnd/",
+        "https://nurl.website/blog/plan-session-ten-minutes/",
+        "https://nurl.website/legal/community-guidelines/",
+        "https://nurl.website/legal/terms/",
+        "https://nurl.website/legal/privacy/",
+        "https://nurl.website/legal/copyright-policy/",
+        "https://nurl.website/legal/other/",
+        "https://nurl.website/legal/licenses/",
+        "https://nurl.website/legal/paid-services/",
+        "https://nurl.website/legal/acknowledgements/",
+        "https://nurl.website/legal/group-subscription-policy/",
+        "https://nurl.website/legal/off-platform-behavior/",
+        "https://nurl.website/legal/monitization-terms/",
+        "https://nurl.website/legal/data-reporting-metrics/",
+      ],
       filter: (page) =>
         page !== "https://nurl.website/thanks/" &&
         page !== "https://nurl.website/success/" &&
